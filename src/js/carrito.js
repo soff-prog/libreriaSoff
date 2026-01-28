784aa39390b90a40249d0a9cd5fcaf2fbc721c80
soff <sof.polette@gmail.com>
2026-01-28 08:11:03 -0500

    carrito de compras

---
+++ src/js/carrito.js
@@ -0,0 +1,42 @@
+import '../css/style.css'; 
+import 'flowbite';
+
+
+const lista = document.getElementById("lista-carrito");
+const totalPago= document.getElementById("total-compra");
+
+//traer la informacion de la memoria del localStorege
+let libroCarrito = JSON.parse(localStorage.getItem("carrito-libros"))||[];
+
+const mostrarCarrito= ()=>{
+    lista.innerHTML = "";
+    let total = 0;
+
+    libroCarrito.forEach((libro, index) => {
+        total += parseFloat(libro.precio);
+
+        //CARRITO
+        lista.innerHTML += `
+            <div class="flex justify-between items-center border-b border-slate-700 py-4">
+                <p class="font-bold">${libro.titulo}</p>
+                <div class="flex items-center gap-4">
+                    <span class="text-orange-900 font-bold">$${libro.precio}</span>
+                    <button data-index="${index}" class="btn-eliminar text-red-500 text-xs">Eliminar</button>
+                </div>
+            </div>
+        `;
+    });
+totalPago.innerText=`$${total.toFixed(2)}`;
+;}
+
+lista.addEventListener("click", (e) => {
+    const boton = e.target.closest(".btn-eliminar");
+    if(boton){
+        const index = parseInt(boton.dataset.index);
+        //Eliminar el libro del carrito basandose en la posicion seleccionada
+        librosCarrito.splice(index, 1); //splice es un metodo para eliminar
+        //actualizar los datos
+        localStorage.setItem("carrito-libros", JSON.stringify(librosCarrito));
+    }
+})
+mostrarCarrito();
